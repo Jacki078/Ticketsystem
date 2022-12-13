028b810022c27e3db5f4b3ae898e0ef3146665c7
ArSinka <ArSinka@BFW-G4LSSM2.IT-AB.de>
2022-12-13 13:06:08 +0100

    UserTicket v1.0

--- Ticket_System/src/Login_Sys/UserTicket.java
+++ Ticket_System/src/Login_Sys/UserTicket.java
@@ -4,10 +4,20 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UserTicket {
 
 	private JFrame frmBenutzerticket;
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
 
 	/**
 	 * Launch the application.
@@ -38,7 +48,7 @@ public class UserTicket {
 	private void initialize() {
 		frmBenutzerticket = new JFrame();
 		frmBenutzerticket.setTitle("Benutzerticket");
-		frmBenutzerticket.setBounds(100, 100, 591, 483);
+		frmBenutzerticket.setBounds(100, 100, 591, 443);
 		frmBenutzerticket.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmBenutzerticket.getContentPane().setLayout(null);
 		
@@ -50,8 +60,55 @@ public class UserTicket {
 		lblPrioLabel_1.setBounds(96, 11, 46, 14);
 		frmBenutzerticket.getContentPane().add(lblPrioLabel_1);
 		
-		JLabel lblBeschreibungLabel = new JLabel("New label");
-		lblBeschreibungLabel.setBounds(183, 11, 46, 14);
+		JLabel lblBeschreibungLabel = new JLabel("T-Beschreibung");
+		lblBeschreibungLabel.setBounds(183, 11, 86, 14);
 		frmBenutzerticket.getContentPane().add(lblBeschreibungLabel);
+		
+		JButton btnAnsehenButton = new JButton("Ansehen");
+		btnAnsehenButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAnsehenButton.setBounds(445, 66, 89, 23);
+		frmBenutzerticket.getContentPane().add(btnAnsehenButton);
+		
+		JButton btnBearbeitenButton_1 = new JButton("Bearbeiten");
+		btnBearbeitenButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBearbeitenButton_1.setBounds(445, 113, 89, 23);
+		frmBenutzerticket.getContentPane().add(btnBearbeitenButton_1);
+		
+		JButton btnErstellenButton_2 = new JButton("Erstellen");
+		btnErstellenButton_2.setBounds(445, 370, 89, 23);
+		frmBenutzerticket.getContentPane().add(btnErstellenButton_2);
+		
+		JButton btnLöschenButton_3 = new JButton("Löschen");
+		btnLöschenButton_3.setBounds(445, 160, 89, 23);
+		frmBenutzerticket.getContentPane().add(btnLöschenButton_3);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 33, 555, 2);
+		frmBenutzerticket.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(10, 357, 555, 2);
+		frmBenutzerticket.getContentPane().add(separator_1);
+		
+		table = new JTable();
+		table.setBounds(10, 46, 46, 300);
+		frmBenutzerticket.getContentPane().add(table);
+		
+		table_1 = new JTable();
+		table_1.setBounds(96, 46, 46, 300);
+		frmBenutzerticket.getContentPane().add(table_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(183, 46, 202, 300);
+		frmBenutzerticket.getContentPane().add(scrollPane);
+		
+		table_2 = new JTable();
+		scrollPane.setViewportView(table_2);
 	}
 }
